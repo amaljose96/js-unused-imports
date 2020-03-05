335909ea423bf4b2f4b2d92e98ed4becb7e95df1
Amal Jose <amal.jose@tesco.com>
2020-03-05 23:44:56 +0530

    Initial commit

---
+++ unusedFiles.py
@@ -0,0 +1,27 @@
+
+import sequences
+
+
+filePaths = [
+                "../../Hackathon-2019/shopping-assistant/src/index.js",
+                ]
+fileQueue = []
+packageImports = {}
+fileImports = {}
+highestDirectory=""
+packageStatuses={}
+fileStatuses={}
+directoryMap={}
+
+sequences.printHeader()
+sequences.prepareFileQueue(filePaths,fileQueue)
+sequences.scanFiles(fileQueue,packageImports,fileImports)
+highestDirectory=sequences.gettingProjectDirectory(fileImports)
+sequences.displayUsedPackages(packageImports)
+sequences.displayUsedFiles(fileImports,highestDirectory)
+packageStatuses=sequences.displayAllPackages(packageImports,highestDirectory)
+fileStatuses=sequences.displayAllFiles(fileImports,highestDirectory,directoryMap)
+sequences.printUnusedPackagesCount(packageStatuses)
+sequences.printUnusedFilesCount(fileStatuses)
+sequences.generateUnusedPackagesJson(packageStatuses)
+sequences.generateUnusedFilesJson(directoryMap)
